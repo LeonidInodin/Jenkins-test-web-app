080e2de6fb0d749595b8a70ef75096a73dccf8d4
Leonid Inodin <leonid_inodin@epam.com>
2023-12-29 10:38:06 +0400

    First commit

---
+++ src/main/java/com/epam/inodin/jenkinstestwebapp/HelloServlet.java
@@ -0,0 +1,68 @@
+package com.epam.inodin.jenkinstestwebapp;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+
+@WebServlet(name = "/helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+    private String POSTGRESQL_URL;
+    private String POSTGRESQL_USERNAME;
+    private String POSTGRESQL_PASSWORD;
+
+    public void init() {
+        POSTGRESQL_URL = System.getenv("POSTGRESQL_URL");
+        POSTGRESQL_USERNAME = System.getenv("POSTGRESQL_USERNAME");
+        POSTGRESQL_PASSWORD = System.getenv("POSTGRESQL_PASSWORD");
+        message = getTextFromDb();
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + getInfoFromServlet() + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public String getTextFromDb() {
+
+        String result;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            Connection connection = DriverManager.getConnection(
+                    POSTGRESQL_URL,
+                    POSTGRESQL_USERNAME,
+                    POSTGRESQL_PASSWORD);
+            Statement stmnt = connection.createStatement();
+            ResultSet rs = stmnt.executeQuery("SELECT text FROM example WHERE id=1");
+            rs.next();
+            result = rs.getString("text");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    public String getInfoFromServlet(){
+        return "Hello from servlet!";
+    }
+
+    public void destroy() {
+    }
+}
---
+++ src/test/java/HelloServletUnitTests.java
@@ -0,0 +1,32 @@
+import com.epam.inodin.jenkinstestwebapp.HelloServlet;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class HelloServletUnitTests {
+
+    HelloServlet helloServlet = new HelloServlet();
+
+    //Test will pass every time
+    @Test
+    void getInfoFromServlet_returnsString() {
+        //given
+        var expectedString = "Hello from servlet!";
+        //when
+        var resultString = helloServlet.getInfoFromServlet();
+        //then
+        assertEquals(expectedString, resultString);
+    }
+
+    //Test won't pass every time
+    /*@Test
+    void getInfoFromServlet_returnsWrongString() {
+        //given
+        var expectedString = "Wrong string";
+        //when
+        var resultString = helloServlet.getInfoFromServlet();
+        //then
+        assertEquals(expectedString, resultString);
+    }*/
+}
